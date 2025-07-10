8c2c949c86f0963917165bb26a544c22c6c765d4
Glen Polo <glen.polo@gmail.com>
2025-07-10 13:31:59 -0400

    ignore unkown properties in file

--- src/main/java/com/example/demo/config/MongoCredentials.java
+++ src/main/java/com/example/demo/config/MongoCredentials.java
@@ -1,10 +1,11 @@
 package com.example.demo.config;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Data;
 
 @Data
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class MongoCredentials {
     private String username;
     private String password;
-    private String someOtherProperty;
 }
